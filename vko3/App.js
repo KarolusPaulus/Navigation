46e8629ea37e3f2f5114ac3628240ad64aae7f1b
KarolusPaulus <karosaarinenn@gmail.com>
2025-01-20 11:21:35 +0200

    App

---
+++ vko3/App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { PaperProvider } from 'react-native-paper';
+import HomeScreen from './screens/HomeScreen';
+import SecondScreen from './screens/SecondScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Second" component={SecondScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
---
+++ vko3/components/AppBar.js
@@ -0,0 +1,17 @@
+import HomeScreen from '../screens/HomeScreen';
+import SecondScreen from '../screens/SecondScreen';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          header: (props) => <CustomNavigationBar {...props} />,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
